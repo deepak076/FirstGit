230f59a4825ec1bd05692ee0440ceed729fb6453
deepak076 <deepak19csu076@ncuindia.edu>
2023-07-02 20:19:48 +0530

    second-commit

---
+++ js2/dom.js
@@ -0,0 +1,31 @@
+// // // console.dir(document);
+// // console.log(document.domain);
+// // console.log(document.URL);
+// // console.log(document.title);
+// // // document.title = 123;
+// // console.log(document.doctype);
+// // console.log(document.head);
+// // console.log(document.body);
+// // console.log(document.all);
+// // console.log(document.all[10]);
+// // console.log(document.forms);
+// // console.log(document.links);
+// console.log(document.images);
+// console.log(document.links);
+
+
+// console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerText= 'Goodbye'
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+headerTitle.style.borderBottom = 'solid 3px #000';
+headerTitle.style.color = 'green';
+headerTitle.style.fontWeight = 'bold';
+
+
+// var items= document.getElementsByClassName();
+
+
