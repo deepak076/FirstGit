4840c70cd24f355b9b16218bdb01a99e1eeba35e
deepak076 <83640474+deepak076@users.noreply.github.com>
2023-07-05 20:00:48 +0530

    Create dom5.js

---
+++ js2/dom5.js
@@ -0,0 +1,37 @@
+//traversing the dom 
+var items=document.querySelector('#items');
+// parentNode
+// console.log(items.parentNode);
+// items.parentNode.style.backgroundcolor= '#f4f4f4';
+// console.log(items.parentNode.parentNode);
+
+//parentElement
+// console.log(items.parentElement);
+// items.parentElement.style.backgroundcolor= '#f4f4f4';
+// console.log(items.parentElement.parentElement);
+
+//Childnodes
+// console.log(items.childNodes);
+
+
+// console.log(items.children);
+// console.log(items.children[1]);
+// items.children[1].style.backgroundColor= 'yellow';
+ 
+// //first child
+// console.log(items.firstChild);
+
+// //first element child
+// console.log(items.lastElementChild);
+// items.lastElementChild.textContent = 'Hello 4';
+
+
+
+// //last child
+// console.log(items.lastChild);
+
+// //last element child
+// console.log(items.lastElementChild);
+// items.lastElementChild.textContent = 'Hello 4';
+
+
