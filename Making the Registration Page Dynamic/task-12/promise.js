d5cc8685222535543ef33f0c00432707f5fc19c2
deepak076 <83640474+deepak076@users.noreply.github.com>
2023-08-07 17:05:19 +0530

    Update promise.js

--- Making the Registration Page Dynamic/task-12/promise.js
+++ Making the Registration Page Dynamic/task-12/promise.js
@@ -1,48 +1,71 @@
-let lastActivityTime = new Date("2023-08-07T10:00:00"); // Replace with your desired initial value
-
 function updateLastUserActivityTime(userId) {
-  return new Promise((resolve, reject) => {
-    // Simulate updating the user's last activity time
-    setTimeout(() => {
-      const currentTime = new Date();
-      lastActivityTime = currentTime;
-      resolve('User activity time updated successfully');
-    }, 1000); // Simulating a 1-second delay
-  });
-}
-
-// Function to create a post and update user's last activity time
-function createPost(userId, postContent) {
-  return new Promise((resolve, reject) => {
-    // Log the user's last activity time before creating the post
-    console.log(`Before creating post - Last activity time for user ${userId}:`, lastActivityTime);
-
-    // Simulate creating a post
-    console.log(`User ${userId} created a post: ${postContent}`);
-    
-    // Update user's last activity time
-    updateLastUserActivityTime(userId)
-      .then(result => {
-        console.log(result);
-        // Log the user's last activity time after creating the post
-        console.log(`After creating post - Last activity time for user ${userId}:`, lastActivityTime);
-        resolve();
-      })
-      .catch(error => {
-        console.error(error);
-        reject(error);
-      });
-  });
-}
-
-// Example usage
-const userId = 123;
-const postContent = "Hello, world!";
-
-createPost(userId, postContent)
-  .then(() => {
-    console.log('Post creation and activity update completed.');
-  })
-  .catch(error => {
-    console.error('Error occurred during post creation and activity update:', error);
-  });
+    return new Promise((resolve, reject) => {
+      // Simulate updating the user's last activity time
+      setTimeout(() => {
+        const success = true; // Replace this with your actual success condition
+        if (success) {
+          resolve('User activity time updated successfully');
+        } else {
+          reject(new Error('Failed to update user activity time'));
+        }
+      }, 1000); // Simulating a 1-second delay
+    });
+  }
+  
+  // Function to create a post and update user's last activity time
+  async function createPost(userId, postContent) {
+    try {
+      // Log the user's last activity time before creating the post
+      console.log(`Before creating post - Last activity time for user ${userId}:`, new Date());
+      
+      // Simulate creating a post
+      console.log(`User ${userId} created a post: ${postContent}`);
+      
+      // Update user's last activity time
+      const result = await updateLastUserActivityTime(userId);
+      console.log(result);
+  
+      // Log the user's last activity time after creating the post
+      console.log(`After creating post - Last activity time for user ${userId}:`, new Date());
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+  
+  // Function to delete a post
+  async function deletePost(postId) {
+    try {
+      // Simulate deleting a post
+      await new Promise(resolve => setTimeout(resolve, 1000)); // Simulating a 1-second delay
+      const success = true; // Replace this with your actual success condition
+      if (success) {
+        return `Post ${postId} deleted successfully`;
+      } else {
+        throw new Error(`Failed to delete post ${postId}`);
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+  
+  // Example usage
+  const userId = 123;
+  const postContent = "Hello, world!";
+  
+  (async () => {
+    try {
+      await createPost(userId, postContent);
+      
+      console.log('Post creation and activity update completed.');
+      
+      const deleteResult = await deletePost('post-id-to-delete');
+      console.log(deleteResult);
+      
+      console.log('Post deletion completed.');
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  })();
+  
